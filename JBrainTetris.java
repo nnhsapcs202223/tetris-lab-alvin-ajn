f8eebb80cc414632c95622402a7ac203d7781c91
Alvin Ng <ajng@stu.naperville203.org>
2023-03-06 15:09:48 -0600

    Working on jbraintetris

--- JBrainTetris.java
+++ JBrainTetris.java
@@ -13,14 +13,34 @@ import javax.swing.event.*;
 public class JBrainTetris extends JTetris
 {
     private Brain brain;
-    
+
     public JBrainTetris(int width, int height)
     {
         super(width, height);
     }
-    
+
     @Override public Container createControlPanel()
     {
-        return super.createControlPanel();
+        Container panel = new Container();
+        panel = super.createControlPanel();
+        JComboBox cb = new JComboBox();
+        ArrayList<Brain> brainList = BrainFactory.createBrains();
+        Brain[] brains = new Brain[brainList.size()];
+        for(int i = 0; i < brains.length; i++)
+        {
+            brains[i] = brainList.get(i);
+        }
+        cb = new JComboBox(brains);
+
+        return panel;
+    }
+
+    public class ComboBox implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            JComboBox cb = (JComboBox)e.getSource();
+            String brainName = (String)cb.getSelectedItem();
+        }
     }
 }
